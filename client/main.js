6e9285af9bdd152645095bff1444a71c8bd04cbd
Justin Permann <scoobs@Justins-MacBook-Pro.local>
2013-02-28 18:56:24 -0700

    small changes

--- client/main.js
+++ client/main.js
@@ -9,25 +9,21 @@ function bindHandlers(){
     
     //Buttons
     $('.newGame').click(function(){
-        console.log("Inside New Game");
         $('.gameScreen').html(render('newGame'));
         bindHandlers();
     });
 
     $('.highScores').click(function(){
-        console.log("Inside High Scores");
         $('.gameScreen').html(render('highScores', {names: ["Justin", "Dallin", "Tarah"]}));
         bindHandlers();
     });
 
     $('.settings').click(function(){
-        console.log("Inside settings");
         $('.gameScreen').html(render('settings'));
         bindHandlers();
     });
 
     $('.credits').click(function(){
-        console.log("Inside Credits");
         $('.gameScreen').html(render('credits'));
         bindHandlers();
     });
@@ -38,7 +34,6 @@ function bindHandlers(){
     });
     
     $('.back').click(function(){
-        console.log("You clicked the back button! ZOMG ");
         $('.gameScreen').html(render('mainMenu'));
         bindHandlers();
     });  
